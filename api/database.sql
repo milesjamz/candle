63052511fde753691dab7eb8144a2cc39b00566f
Miles Marillo <mjm03@hampshire.edu>
2021-08-21 19:09:42 -0400

    first commit

---
+++ api/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE candle;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(20),
+)
---
+++ api/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const PORT = 8080;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/user/:id', (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    if (!name) {
+        res.status(418).send({ message: 'We need a username! '})
+    }
+
+    res.send({
+        user: `This is ${name}, who is user ${id}.`
+    })
+})
+
+app.get('/tshirt', (req,res) => {
+res.status(200).send({
+    tshirt: '👕',
+    size: 'large'
+})
+})
+
+// app.get('/user/:id', (req,res) => {
+//     const {id} = req.params;
+
+//     res.send({
+//         user:
+//     })
+// })
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
