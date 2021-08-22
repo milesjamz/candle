4d77ee4915e27495a0f8dce2f077b40da675a68a
Miles Marillo <mjm03@hampshire.edu>
2021-08-22 15:38:28 -0400

    login

--- client/src/Login.js
+++ client/src/Login.js
@@ -18,8 +18,20 @@ function Login() {
         <div>
             <h2>LOG IN</h2>
             <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email:</label><input type='email'/><br/>
-            <label htmlFor="password">Password:</label><input type='password'/><br/>
+            <label htmlFor="email">Email:</label>
+                <input type="email"
+                       value={user.email}
+                       name="email"
+                       onChange={e => setUser({ ...user, email: e.target.value}) }
+                       />
+                       
+                       <br/>
+            <label htmlFor="password">Password:</label>
+                <input type='password'
+                       value={user.password}
+                       name="password"
+                       onChange={e => setUser({ ...user, password: e.target.value}) }
+                       /><br/>
             </form>
         </div>
     )
