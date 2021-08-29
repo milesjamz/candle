ac1fa81552f83b43fea93ab3d13b0013272d1af6
Miles Marillo <mjm03@hampshire.edu>
2021-08-29 13:35:37 -0400

    login + signup

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,10 +8,53 @@ function Login() {
     });
 
 
+    const getProfileFromServer = () => {
+        // --- gets your profile from the server ---
+            if (localStorage.token) {
+              fetch("http://localhost:3000/api/v1/profile", {
+                headers: {
+                  Authorization: `Bearer ${localStorage.token}`
+                }
+              })
+                .then(res => res.json())
+                .then(profileData => {
+                    console.log(profileData)
+                //   this.setState({
+                //     current_user: profileData.user,
+                //     logged_in: true
+                //   });
+                });
+            }
+          };
+
+
+
+
     const handleSubmit = evt => {
         evt.preventDefault();
-        alert(`Submitting ${user.name}`)
-    }
+        alert(`Submitting ${user.email}`)
+        console.log(user)
+            // --- logs in the user ---
+                fetch("http://localhost:3000/api/v1/login", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                  },
+                  body: JSON.stringify({user: user})
+                })
+                  .then(res => res.json())
+                  .then(data => {
+                      console.log(data)
+                    if (data.jwt) {
+                      localStorage.setItem("token", data.jwt);
+                      getProfileFromServer()
+                    console.log('yes')
+                    } else {
+                      alert("Your info doesn't match our records; please try again.");
+                    }
+                  });
+            }
 
 
     return (
@@ -32,6 +75,7 @@ function Login() {
                        name="password"
                        onChange={e => setUser({ ...user, password: e.target.value}) }
                        /><br/>
+                       <input type='submit' />
             </form>
         </div>
     )
