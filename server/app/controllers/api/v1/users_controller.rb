5368e237d22381f483dbc01889e1a44d592038b6
Miles Marillo <mjm03@hampshire.edu>
2021-08-29 16:12:33 -0400

    login + signup

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-      params.require(:user).permit(:username, :password, :birthday, :email, :avatar)
+      params.require(:user).permit(:username, :password_digest, :birthday, :email, :avatar)
     end
   end
   
