e1fe227646ab9f2028ea36af6e672043a8539ba3
Miles Marillo <mjm03@hampshire.edu>
2021-09-01 17:49:03 -0400

    routes

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,7 +1,5 @@
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
     Link
   } from "react-router-dom";
 
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      # resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
-      resources :users do 
+      resources :users, only: [:create] do 
         member do
           post 'follow'
           post 'unfollow'
