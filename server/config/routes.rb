0ab9b970fe03af0242c9f5669711dceacd752f15
Miles Marillo <mjm03@hampshire.edu>
2021-09-03 15:07:33 -0400

    routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       # resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
-      resources :users, only: [:create] do 
+      resources :users do 
         member do
           post 'follow'
           post 'unfollow'
