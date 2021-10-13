1b3e010f1f265985557bc4239315028679e7bc53
Miles Marillo <mjm03@hampshire.edu>
2021-10-13 19:25:28 -0400

    minor changes

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -11,7 +11,7 @@ function Landing() {
             <h2>Keep Track of Moments That Matter</h2>
             <p>An easy way to organize you and your friends' birthdays, make and record
             memories, and find new friends.</p>
-            <ul>
+            <ul id='landingList'>
             <li><Link to='/login'><button className='loginBtn'>Log In</button></Link></li>
             <li><Link to='/signup'><button className='loginBtn'>Sign Up</button></Link></li>
             </ul>
--- client/src/UserCard.js
+++ client/src/UserCard.js
@@ -44,20 +44,3 @@ return <button id={props.user.id} key={props.user.id} onClick={deleteMe}> Unfoll
 }
 
 export default UserCard;
-
-//8th floor.
-//mother baby is on 13th floor
-//NICU is on 9th floor.
-
-//when you arrive, come to 1st bw 32nd and 33rd port cochere. GO HERE.
-//there's valet parking hut to 
-
-//walk down BLUE pathway.
-//first place to go is triage room most likely
-//first assessments, to prioritise flow of patients. there are 12 labor/delivery rooms.
-//this can take time, screening, health questions, etc. from 45 mins to several hours.
-//covid test booo
-//l+d room comes next.  
-//bring masKs etc.
-//
-//A
